949a71efd18df099a2102a7d063ae483ad7b47fd
jokangmin <jomin5151@gmail.com>
2024-11-15 14:35:23 +0900

    first commit

---
+++ src/main/frontreact/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Spring Boot + React + NCP</h1>
+      <h3>카운트 : 1</h3>
+      <img src='./image/3.jpeg' alt=''/>
+    </div>
+  );
+};
+
+export default App;
